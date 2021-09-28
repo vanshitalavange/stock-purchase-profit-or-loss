3e7fc786c7bbca44b91b38ffd2622f236961dc85
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-28 21:40:14 +0530

    structure

---
+++ app.js
@@ -0,0 +1,27 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocksquantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputEl = document.querySelector("#output");
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+    console.log("loss", loss, lossPercentage);
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+    console.log("profit", profit, profitPercentage);
+  } else {
+    console.log("No profit no loss");
+  }
+}
+function checkProfitOrLoss() {
+  var initial = Number(initialPrice.value);
+  var quantity = Number(stocksQuantity.value);
+  var current = Number(currentPrice.value);
+  calculateProfitAndLoss(initial, quantity, current);
+}
+
+submitBtn.addEventListener("click", checkProfitOrLoss);
